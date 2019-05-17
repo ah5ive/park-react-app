20d7846babcfa258afb2fc5f8ea774fb84ad1a58
Jason Wong <jason@enginestudios.sg>
2019-05-17 18:43:45 +0800

    flexbox css

--- src/component/result/result.js
+++ src/component/result/result.js
@@ -11,7 +11,7 @@ class result extends React.Component {
          console.log("===FilterResult===",this.props.filterResultProps);
          //console.log("xxx result props xxx",this.props.resultProps);
          const addResults = this.props.filterResultProps.map((carpark, index)=>{
-            return  <React.Fragment><div key={index} className="searchResultList"><p>Address: <br/>{carpark.address}</p><button className="resultBtn" onClick={()=>this.props.checkAvailabiltyHandlerProps(carpark,index)}>Check Availablity</button></div></React.Fragment>
+            return  <React.Fragment key={index}><div  className="searchResultList"><p className="addressColor">Address: </p><p>{carpark.address}</p><button className="resultBtn" onClick={()=>this.props.checkAvailabiltyHandlerProps(carpark,index)}>Check Availablity</button></div></React.Fragment>
          })
         return (
                 <div>
