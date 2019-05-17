d076a6a3d7a71670eb5a541712521b0101b62783
Jason Wong <jason@enginestudios.sg>
2019-05-17 16:27:11 +0800

    Updates

---
+++ precache-manifest.8685a9fa7e46d58553169affcaef4d99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09054728222a3329e4f5",
+    "url": "/parking-react-app/static/css/main.f5a79d9c.chunk.css"
+  },
+  {
+    "revision": "09054728222a3329e4f5",
+    "url": "/parking-react-app/static/js/main.75603015.chunk.js"
+  },
+  {
+    "revision": "1f6149c86e41daa2ac2b",
+    "url": "/parking-react-app/static/js/runtime~main.c9a8439c.js"
+  },
+  {
+    "revision": "dfc2dca4e31f272728cc",
+    "url": "/parking-react-app/static/js/2.a6844af4.chunk.js"
+  },
+  {
+    "revision": "4b548a1d818916ce40c4c625d1778d9f",
+    "url": "/parking-react-app/index.html"
+  }
+];
