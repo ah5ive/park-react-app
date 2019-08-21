7719c6224ed0435bd195066f93811b60f1f3a62d
Jason Wong <jason@enginestudios.sg>
2019-08-21 22:19:15 +0800

    npm install redux and use createStore, wrote a sample reducer.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+const store = createStore(reducer);
+// import * as serviceWorker from './serviceWorker';
+//add reducer
+function reducer(){
+    return 'State';
+}
+
+console.log(store.getState(),"GETSTATE")
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
     ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
